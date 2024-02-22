8e250284bb0124dda988b3e6993f1c4d60aceb3f
nicklin-sjsu <nicklin1219@gmail.com>
2024-02-21 19:19:19 -0800

    fix comment

--- src/services/instagram.js
+++ src/services/instagram.js
@@ -7,13 +7,15 @@ exports.authInstagram = (req, res) => {
   );
 };
 
-// Twitter Authentication Callback
+// Instagram Authentication Callback
 exports.authInstagramCallback = (req, res) => {
   // Authorization Codes are valid for 1 hour and can only be used once.
   res.redirect(
     `https://graph.facebook.com/v19.0/oauth/access_token?client_id=${process.env.FACEBOOK_CLIENT_ID}&redirect_uri=${process.env.FACEBOOK_REDIRECT_URI}&client_secret=${process.env.FACEBOOK_CLIENT_SECRET}&code=${req.query.code}`
   );
 };
+
+// Instagram Access Token Callback
 exports.authInstagramAccessToken = (req, res) => {
   // Authorization Codes are valid for 1 hour and can only be used once.
   console.log(req.body);
