0fa8969bbd5b34cd6bfbde84fb69f94fc06a43cb
nihalwashere <57855059+nihalwashere@users.noreply.github.com>
2024-05-10 16:46:00 -0700

    fix: uploads

--- src/controllers/v1/posts.js
+++ src/controllers/v1/posts.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, path.join(__dirname, "/uploads/"));
   },
   filename(req, file, cb) {
     const fileName = `${Date.now()}-${file.originalname}`;
